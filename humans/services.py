6ac66b062001993d25148e09baf42cfdb64c3e9c
Mikhail Vashchuk <mikhailvashchuk505@gmail.com>
2026-07-29 14:10:26 +0300

    Validate passwords during human registration

--- humans/services.py
+++ humans/services.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.password_validation import validate_password
 from django.db import transaction
 
 from accounts.models import User
@@ -6,7 +7,18 @@ from .models import Human
 
 
 @transaction.atomic
-def register_human(*, first_name, last_name, email, password,):
+def register_human(*, first_name, last_name, email, password):
+    user_for_validation = User(
+        first_name=first_name,
+        last_name=last_name,
+        email=User.objects.normalize_email(email),
+    )
+
+    validate_password(
+        password,
+        user=user_for_validation,
+    )
+
     user = User.objects.create_user(
         first_name=first_name,
         last_name=last_name,
--- humans/tests.py
+++ humans/tests.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from accounts.models import User
@@ -12,7 +13,7 @@ class RegistrationTests(TestCase):
             first_name="Mikhail",
             last_name="Vashchuk",
             email="mikhailvashchuk505@gmail.com",
-            password="test-password",
+            password="gd2g78d626i",
         )
 
         self.assertEqual(User.objects.count(), 1)
@@ -25,5 +26,17 @@ class RegistrationTests(TestCase):
         self.assertEqual(human.user.first_name, "Mikhail")
         self.assertEqual(human.user.last_name, "Vashchuk")
         self.assertTrue(
-            human.user.check_password("test-password")
+            human.user.check_password("gd2g78d626i")
         )
+
+    def test_register_human_raises_when_password_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            register_human(
+                first_name="Mikhail",
+                last_name="Vashchuk",
+                email="mikhailvashchuk505@gmail.com",
+                password="12345678",
+            )
+
+        self.assertEqual(User.objects.count(), 0)
+        self.assertEqual(Human.objects.count(), 0)
